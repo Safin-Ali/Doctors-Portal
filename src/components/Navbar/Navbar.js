bda5be58cc1c5548c63fcc94887db0337e07a83d
Safin-Ali <safin.ali.7205@gmail.com>
2022-11-14 12:38:03 +0600

    responsive navbar created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
     return (
-        <nav className={`container flex p-3 items-center mx-auto`}>
+        <nav className={`container flex-wrap justify-between flex p-3 items-center mx-auto`}>
             {/* Brand Logo/Text */}
-            <div className={`basis-1/2`}>
+            <div className={``}>
                 <h1 className={`p-1.5`}>Doctors Portal</h1>
             </div>
-            
+
+        {/* Bars Icon */}
+            <div className={``}>
+                <FaBars className={`text-2xl cursor-pointer block lg:hidden`}></FaBars>
+            </div>
+
             {/* Nav Link */}
-            <div className={`flex basis-1/2`}>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Home</Link>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>About</Link>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Appoinments</Link>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Reviews</Link>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Contact Us</Link>
-                <Link className={`mx-2 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Login</Link>
+            <div className={`basis-full lg:basis-auto lg:flex overflow-hidden justify-center text-center`}>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Home</Link>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>About</Link>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Appoinments</Link>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Reviews</Link>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Contact Us</Link>
+                <Link className={`block border-b mx-2 lg:border-0 text-black p-1.5 rounded-md hover:bg-slate-800 duration-150 hover:text-[#D4D9E3]`} to={`/`}>Login</Link>
             </div>
         </nav>
     );
